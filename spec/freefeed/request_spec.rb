dad2fd8db485a3d2552f7840295c1aad68ee9e10
Alex Musayev <alex.musayev@gmail.com>
2019-11-28 17:27:32 +0300

    Test HTTP errors handling

--- spec/freefeed/request_spec.rb
+++ spec/freefeed/request_spec.rb
@@ -6,13 +6,33 @@ describe Freefeed::Request do
   subject(:request) { described_class.new(request_method, path, options) }
 
   let(:request_method) { :get }
-  let(:path) { "/v2/users/whoami" }
+  let(:path) { "/example" }
   let(:options) { {} }
 
-  before do
-    stub_request(:get, "https://freefeed.net/v2/users/whoami")
-      .to_return(body: {}.to_json)
+  let(:expected_url) { "https://freefeed.net/example" }
+
+  context "when respose is successfull" do
+    before { stub_request(:get, expected_url).to_return(body: {}.to_json) }
+    it { request.call }
   end
 
-  it { request.call }
+  context "when bad request" do
+    before { stub_request(:get, expected_url).to_return(status: 400) }
+    it { expect { request.call }.to raise_error(Freefeed::Error::BadRequest) }
+  end
+
+  context "when unathorized" do
+    before { stub_request(:get, expected_url).to_return(status: 401) }
+    it { expect { request.call }.to raise_error(Freefeed::Error::Unauthorized) }
+  end
+
+  context "when forbidden" do
+    before { stub_request(:get, expected_url).to_return(status: 403) }
+    it { expect { request.call }.to raise_error(Freefeed::Error::Forbidden) }
+  end
+
+  context "when forbidden" do
+    before { stub_request(:get, expected_url).to_return(status: 500) }
+    it { expect { request.call }.to raise_error(Freefeed::Error::ServerError) }
+  end
 end
