ceb3084eaa25acc27858f0b18be72773a6278816
Alex Musayev <alex.musayev@gmail.com>
2019-11-28 21:36:05 +0300

    Test logger option

--- spec/freefeed/client_spec.rb
+++ spec/freefeed/client_spec.rb
@@ -6,14 +6,17 @@ describe Freefeed::Client do
   describe "config" do
     subject(:config) { described_class.config }
 
-    let(:token) { "Banana" }
+    let(:token) { "[Token]" }
+    let(:logger) { "[Logger]" }
 
     before do
       described_class.configure do |config|
         config.token = token
+        config.logger = logger
       end
     end
 
     it { expect(config.token).to eq(token) }
+    it { expect(config.logger).to eq(logger) }
   end
 end
