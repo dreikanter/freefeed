54a55ff2cf9ce5b6112cc4fa5f628f782ccc4321
Alex Musayev <alex.musayev@gmail.com>
2019-11-28 17:16:15 +0300

    Add simplecov and webmock to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
-require "bundler/setup"
+require "simplecov"
+
+SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter]
+
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
 require "freefeed"
+require "rspec"
 require "pry"
+require "webmock/rspec"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
+  config.color = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
