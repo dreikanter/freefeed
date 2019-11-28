fc8bcb337ae26ea9173cd5a27a00589419171c30
Alex Musayev <alex.musayev@gmail.com>
2019-11-28 21:35:40 +0300

    Add method description block

--- spec/freefeed/users_spec.rb
+++ spec/freefeed/users_spec.rb
@@ -7,41 +7,43 @@ describe Freefeed::Client do
 
   let(:token) { "Banana" }
 
-  context "when token is valid" do
-    let(:response_body) { file_fixture("whoami.json") }
+  describe "#whoami" do
+    context "when token is valid" do
+      let(:response_body) { file_fixture("whoami.json") }
 
-    before do
-      described_class.configure do |config|
-        config.token = token
+      before do
+        described_class.configure do |config|
+          config.token = token
+        end
+
+        stub_request(:get, "https://freefeed.net/v2/users/whoami")
+          .with(
+            headers: {
+              "Accept" => "*/*",
+              "Authorization" => "Bearer #{token}",
+              "Host" => "freefeed.net"
+            }
+          )
+          .to_return(
+            body: response_body,
+            headers: {
+              "Content-Type" => "application/json"
+            }
+          )
       end
 
-      stub_request(:get, "https://freefeed.net/v2/users/whoami")
-        .with(
-          headers: {
-            "Accept" => "*/*",
-            "Authorization" => "Bearer #{token}",
-            "Host" => "freefeed.net"
-          }
-        )
-        .to_return(
-          body: response_body,
-          headers: {
-            "Content-Type" => "application/json"
-          }
-        )
+      it { expect(response).to be_a(Hash) }
     end
 
-    it { expect(response).to be_a(Hash) }
-  end
+    context "when token is not valid" do
+      let(:error_response) { { "err" => "..." } }
 
-  context "when token is not valid" do
-    let(:error_response) { { "err" => "..." } }
+      before do
+        stub_request(:get, "https://freefeed.net/v2/users/whoami")
+          .to_return(status: 401, body: error_response.to_json)
+      end
 
-    before do
-      stub_request(:get, "https://freefeed.net/v2/users/whoami")
-        .to_return(status: 401, body: error_response.to_json)
+      it { expect { response }.to raise_error(Freefeed::Error::Unauthorized) }
     end
-
-    it { expect { response }.to raise_error(Freefeed::Error::Unauthorized) }
   end
 end
