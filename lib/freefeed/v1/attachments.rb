dc773cee4f021feae61ec6832ef433de4b0fc567
Alex Musayev <alex.musayev@gmail.com>
2019-12-01 15:38:07 +0300

    Add attachments endpoint

---
+++ lib/freefeed/v1/attachments.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "freefeed/request"
+
+module Freefeed
+  module V1
+    module Attachments
+      def create_attachment(source)
+        options = { form: { file: file(source) } }
+        authenticated_request(:post, "/v1/attachments", options)
+      end
+
+      private
+
+      def file(source)
+        HTTP::FormData::File.new(source, content_type: "image/png")
+      end
+    end
+  end
+end
---
+++ spec/freefeed/v1/attachments_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe Freefeed::V1::Attachments do
+  include_context "client"
+
+  describe "request" do
+    subject(:response) { client.create_attachment(file) }
+
+    let(:file) { file_fixture("100x100.png") }
+
+    before do
+      stub_request(:post, "https://george.freefeed.net/v1/attachments")
+        .with(
+          headers: {
+            "Accept" => "*/*",
+            "Authorization" => "Bearer #{token}",
+            "Content-Type" => %r{^multipart/form-data}
+          }
+        )
+        .to_return(
+          body: file_fixture("v1/attachments/create_attachment.json").read,
+          headers: {
+            "Content-Type" => "application/json"
+          }
+        )
+    end
+
+    it { expect(response.status).to eq(200) }
+    it { expect(response.parse).to be_a(Hash) }
+  end
+end
