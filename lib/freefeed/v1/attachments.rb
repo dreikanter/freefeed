382f04b937a7ccd0122afb7f479911d8eb7ad71d
Alex Musayev <alex.musayev@gmail.com>
2019-12-04 01:35:03 +0300

    Support explicit content type definition

--- lib/freefeed/v1/attachments.rb
+++ lib/freefeed/v1/attachments.rb
@@ -8,18 +8,19 @@ module Freefeed
     module Attachments
       # @param [String, Pathname, IO] source could by a file path
       # or an IO object
-      def create_attachment(source)
-        options = { form: { file: file(source) } }
+      def create_attachment(source, content_type: nil)
+        options = { form: { file: file(source, content_type) } }
         authenticated_request(:post, "/v1/attachments", options)
       end
 
       private
 
-      def file(source)
-        HTTP::FormData::File.new(source, content_type: content_type(source))
+      def file(source, content_type)
+        content_type ||= detect_content_type(source)
+        HTTP::FormData::File.new(source, content_type: content_type)
       end
 
-      def content_type(source)
+      def detect_content_type(source)
         return MimeMagic.by_magic(source) if source.is_a?(IO)
         MimeMagic.by_path(source.to_s)
       end
--- spec/freefeed/v1/attachments_spec.rb
+++ spec/freefeed/v1/attachments_spec.rb
@@ -6,31 +6,53 @@ describe Freefeed::V1::Attachments do
   include_context "client"
 
   describe "#create_attachment" do
-    subject(:response) { client.create_attachment(source) }
+    context "with implicit content type" do
+      subject(:response) { client.create_attachment(source) }
+
+      before do
+        stub_request(:post, "#{base_url}/v1/attachments")
+          .with(
+            headers: {
+              "Accept" => "*/*",
+              "Authorization" => "Bearer #{token}",
+              "Content-Type" => %r{^multipart/form-data}
+            }
+          )
+          .to_return(
+            body: file_fixture("v1/attachments/create.json").read,
+            headers: { "Content-Type" => "application/json" }
+          )
+      end
+
+      context "with an IO object" do
+        let(:source) { file_fixture("100x100.png") }
+        it_should_behave_like("json hash response")
+      end
+
+      context "with a path" do
+        let(:source) { file_fixture("100x100.png").path }
+        it_should_behave_like("json hash response")
+      end
+    end
+  end
+
+  context "with explicit content type" do
+    subject(:response) do
+      client.create_attachment(source, content_type: content_type)
+    end
+
+    let(:source) { file_fixture("100x100.png") }
+    let(:content_type) { "image/jpg" }
 
     before do
-      stub_request(:post, "https://george.freefeed.net/v1/attachments")
-        .with(
-          headers: {
-            "Accept" => "*/*",
-            "Authorization" => "Bearer #{token}",
-            "Content-Type" => %r{^multipart/form-data}
-          }
-        )
+      stub_request(:post, "#{base_url}/v1/attachments")
+        .with { |request| request.body.match?(/Content-Type: #{content_type}/) }
         .to_return(
-          body: file_fixture("v1/attachments/create_attachment.json").read,
+          body: file_fixture("v1/attachments/create.json").read,
           headers: { "Content-Type" => "application/json" }
         )
     end
 
-    context "with an IO object" do
-      let(:source) { file_fixture("100x100.png") }
-      it_should_behave_like("json hash response")
-    end
-
-    context "with a path" do
-      let(:source) { file_fixture("100x100.png").path }
-      it_should_behave_like("json hash response")
-    end
+    it_should_behave_like("json hash response")
   end
 end
